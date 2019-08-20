7c3473078e004d257c3e12997cb489a9c344abe1
veszeng <335405682@qq.com>
2019-08-20 16:59:06 +0800

    zeng

---
+++ JavaTest/appClientModule/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.print("java,hello");
+		System.out.print("123");
+		System.out.print("qeefvf");
+	}
+
+	public class Result {
+		int add;
+		int sub;
+	};
+
+	public Main() {
+		super();
+	}
+}
